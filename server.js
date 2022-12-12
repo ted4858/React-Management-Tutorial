7961c27d76db0efb531a8395b31a6059e3c9259b
ted4858 <ted4858@naver.com>
2022-12-12 17:29:23 +0900

    Add Customer Delete Module

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const upload = multer({dest: './upload'});
 
 app.get('/api/customers', (req, res) => {
     connection.query(
-      "SELECT * FROM CUSTOMER",
+      "SELECT * FROM CUSTOMER WHERE isDeleted = 0",
       (err, rows, fields) => {
         res.send(rows.rows);
       }
@@ -35,7 +35,7 @@ app.get('/api/customers', (req, res) => {
 app.use('/image', express.static('./upload'));
 
 app.post('/api/customers', upload.single('image'), (req, res) => {
-  let sql = 'INSERT INTO CUSTOMER(image, name, birthday, gender, job) VALUES ($1, $2, $3, $4, $5)';
+  let sql = 'INSERT INTO CUSTOMER(image, name, birthday, gender, job, createdDate, isDeleted) VALUES ($1, $2, $3, $4, $5, now(), 0)';
   let image = '/image/' + req.file.filename;
   let name = req.body.name;
   let birthday = req.body.birthday;
@@ -47,6 +47,16 @@ app.post('/api/customers', upload.single('image'), (req, res) => {
         res.send(rows.rows);
       }
     );
+});
+
+app.delete('/api/customers/:id', (req, res) => {
+  let sql = 'UPDATE CUSTOMER SET isDeleted = 1 WHERE id = $1';
+  let params = [req.params.id];
+  connection.query(sql, params,
+    (err, rows, fields) => {
+      res.send(rows.rows);
+    }
+  )
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
